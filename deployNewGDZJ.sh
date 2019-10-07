cd9eae5685d5424f4bc4d7ebb3399419541df8c7
lwq2019 <360994730@qq.com>
2019-10-07 14:32:37 +0800

    部署新广电组件脚本

---
+++ deployNewGDZJ.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wget   -O  /home/oristartech/gdzj_test.tar.gz  ftp://oriftp:Ori2Ftppw@58.62.144.229/tmp/gdzj_test.tar.gz
+cd  /home/oristartech/ ;  
+tar   zxvf      gdzj_test.tar.gz ;   
+cd  gdzj_test/
+keyIP=`cat /usr/local/GYStandardServer/config/config.main |awk '/NOTICE_LOCAL_ADDRESS/{print $3}'`
+
+[[ -z $keyIP ]] && {
+	echo "获取[NOTICE_LOCAL_ADDRESS]IP失败,脚本退出"
+	exit 1
+}|| {
+	sed "s@NOTICE_LOCAL_ADDRESS =.*@NOTICE_LOCAL_ADDRESS = $keyIP@" config/config.main 
+	./start.sh   release
+	echo  "* * * * *  root  /home/oristartech/gdzj_test/check_gys.sh" >>/etc/crontab
+	echo "退出并恢复旧组件,请执行：./exit.sh "	
+}
+
+
